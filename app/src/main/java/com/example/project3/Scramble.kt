0d10126b87549bcb7cfd6902e25deb5896d528f1
maggie <maggiez33@nycstudents.net>
2024-12-17 09:32:16 -0500

    shuffled worked

--- app/src/main/java/com/example/project3/Scramble.kt
+++ app/src/main/java/com/example/project3/Scramble.kt
@@ -1,6 +1,7 @@
 package com.example.project3
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -38,13 +39,15 @@ class Scramble : Fragment() {
         word = mainActivity?.fileData?.random().toString()
         input = view.findViewById(R.id.scrambleGuess)
 
-        scrambled.text = shuffle(word)
+
+
+        val sArr = word.toCharArray()
+        sArr.shuffle()
+        word = StringBuilder().append(sArr).toString()
+        scrambled.text = word
+        Log.i("Scramble", "${scrambled.text}")
     }
 
-    fun shuffle(string: String): CharArray {
-        var wordarr = string.toCharArray()
-        wordarr.shuffle()
-        return wordarr
-    }
+
 
 }
